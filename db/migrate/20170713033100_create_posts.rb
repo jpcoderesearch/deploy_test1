5d90b04fad6262e82269fe691cd191b78dd9eecd
jpcoderesearch <jpcoderesearch@gmail.com>
2017-07-13 09:03:38 +0530

    add post

---
+++ db/migrate/20170713033100_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.string :Name
+      t.text :Description
+
+      t.timestamps
+    end
+  end
+end
